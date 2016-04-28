14edd53e1d97837b3c41c52fe3e3060a72e1773e
Emilio Tirado <emilio.tirado@blanclink.com>
2016-04-28 10:01:28 -0430

    Manasa and stones fixed

--- src/ManasaAndStones.java
+++ src/ManasaAndStones.java
@@ -16,18 +16,29 @@ public class ManasaAndStones {
             if(n == 1){
                 System.out.println(0);
             }else{
-                Set<Integer> result = new LinkedHashSet<>();
-                for(int k = n-1; k >= 0; k--){
-                    result.add((a*k) + (b * (n-k-1)));
-                }
-                Iterator<Integer> it = result.iterator();
-                System.out.print(it.next());
-
-                for(; it.hasNext();){
-                    System.out.printf(" %d", it.next());
-                }
-                System.out.println();
+//                for(int i=1; i<=n; ++i){
+//                    f(i, a, b);
+//                }
+                f(n, a, b);
             }
         }
+    }
+
+    public static void f(int n, int a, int b){
+        Set<Integer> result = new TreeSet<>();
+
+        for(int k = 1; k <= n; k++){
+            int c1 = n-k,
+                    c2 = k-1;
+            result.add((a*c1) + (b * c2));
+        }
+
+        Iterator<Integer> it = result.iterator();
+        System.out.print(it.next());
+
+        for(; it.hasNext();){
+            System.out.printf(" %d", it.next());
+        }
+        System.out.println();
     }
 }
