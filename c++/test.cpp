31a13df1a2d1440edab5eb119ca4ad477b33aae6
Emilio Tirado <emilio.tirado@blanclink.com>
2016-05-21 16:18:36 -0430

    adding c++ directory

---
+++ c++/test.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	return 0;
+}
